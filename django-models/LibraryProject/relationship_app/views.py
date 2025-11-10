549a66035432af97eae5504dc66ad90d27650c06
Foster Boadi <102637661+fosterb1@users.noreply.github.com>
2025-11-10 10:21:01 +0000

    making corrections to models and urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -110,6 +110,12 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add all books from this library to the context
+        context['books'] = Book.objects.filter(library=self.object)
+        return context
+
 class LibraryListView(ListView):
     model = Library
     template_name = 'relationship_app/library_list.html'
