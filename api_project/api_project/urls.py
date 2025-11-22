dfefe9da4e1b98780dcf9743843ce254ae1b02f4
Foster Boadi <102637661+fosterb1@users.noreply.github.com>
2025-11-22 00:21:20 +0000

    feat: Implement authentication and permissions

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api-token-auth/', auth_views.obtain_auth_token, name='api_token_auth'),
 ]
+    
