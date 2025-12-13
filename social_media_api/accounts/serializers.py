0299413e50ec3e866e0e4eb0c29d8f83f11f64ce
Foster Boadi <102637661+fosterb1@users.noreply.github.com>
2025-12-13 20:22:41 +0000

    little changes

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework.authtoken.models import Token
 from .models import User
 
 
@@ -23,7 +24,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
+        Token.objects.create(user=user)
         return user
     
 class UserLoginSerializer(serializers.Serializer):
